ce75770b247c6dfbf612ab94eda2475108899f60
Alex <eddings.sharanna@gmail.com>
2022-12-18 14:25:36 -0500

    created and imported routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// import route and components
+import { Route, Routes } from "react-router-dom";
+import NavBar from "./components/Nav";
+import Home from "./components/Home";
+import About from "./components/About";
+import Stock from "./components/Stock";
+import Dashboard from "./components/Dashboard";
+
+// css
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/stocks/:symbol" element={<Stock />} />
+        <Route path="/stocks" element={<Dashboard />} />
+      </Routes>
     </div>
   );
 }
