2e25a8c913a4c8e5262e7093b205a0339f087373
Alex <eddings.sharanna@gmail.com>
2022-12-19 16:19:23 -0500

    updates to all files

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,25 +5,13 @@ import stocks from "./stock-data";
 export default function Dashboard() {
   return (
     <>
-      <div className="table-header">
-        <thead>
-          <tr>
-            <th>Company Name</th>
-            <th>Price</th>
-            <th>Change</th>
-            <th>High</th>
-            <th>Low</th>
-            <th>Open</th>
-          </tr>
-        </thead>
-      </div>
       <div className="stock-container">
         <div className="dashboard-stocks">
           {stocks.map((stock) => {
             const { name, symbol, lastPrice, change, high, low, open } = stock;
             return (
               <table>
-                <Link className="link" to={`/stock/${symbol}`}>
+                <Link className="link" key={symbol} to={`/stock/${symbol}`}>
                   <tr>
                     <td>
                       {name} ({symbol})
--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,9 +1,40 @@
-// import componentimport stocks from "./stock-data";
+// import component
 
-export default function Stock(props) {
-  return (
-    <div className="single-stockPage">
-      <h1>Single Stock Page</h1>
-    </div>
-  );
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import stocks from "./stock-data";
+
+export default function Stock() {
+  const [stock, setStock] = useState(null);
+  const { symbol } = useParams();
+
+  useEffect(() => {
+    const getStock = async () => {
+      try {
+        const data = stocks.find((theStock) => theStock.symbol === symbol);
+        setStock(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    getStock();
+  }, [symbol]);
+
+  const loaded = () => {
+    return (
+      <div>
+        <h2>
+          {stock.name} / {stock.symbol}
+        </h2>
+        <h3>${stock.lastPrice}</h3>
+      </div>
+    );
+  };
+
+  const loading = () => {
+    return <h1>...Loading...</h1>;
+  };
+
+  return stock && stock.name ? loaded() : loading();
 }
